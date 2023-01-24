274f8766baf50d3970b23f063e2f70b733b17d65
stevezhengshiqi <zheng.shi.qi@qq.com>
2023-01-23 22:54:49 -0500

    server: Fix quote in server.listen

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ fs.readFile('./public/index.html', (err, data) => {
 // Start the `server` const listening on the port defined by argument in your `port` const. 
 // Put the exact message `Server listening on port ${port}` on the console log. 
     server.listen(port, () => {
-        console.log('Server listening on port ${port}');
+        console.log(`Server listening on port ${port}`);
     });
 });
 // That's it! You're all done!
